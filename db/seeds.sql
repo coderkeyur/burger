20c0d132f2e2f6d9c27774b351e54f9b3281607f
KP <cle.keyur@gmail.com>
2019-04-30 18:11:58 -0400

    schema and seeds sql files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Almond Butter Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lettuce Tomato Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Sandwich", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grilled Cheese", true);
